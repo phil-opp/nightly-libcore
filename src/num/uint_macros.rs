5599c3e4ad706eedce354595aa256289fa7cf668
Philipp Oppermann <dev@phil-opp.com>
2015-11-26 22:05:08 +0100

    Update to 6ef02eff89e3d2a29eab3346bff393821df6e033

---
+++ src/num/uint_macros.rs
@@ -0,0 +1,29 @@
+// Copyright 2012-2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![doc(hidden)]
+
+macro_rules! uint_module { ($T:ty, $T_SIGNED:ty, $bits:expr) => (
+
+
+#[allow(missing_docs)]
+pub const BITS : usize = $bits;
+
+#[allow(missing_docs)]
+pub const BYTES : usize = ($bits / 8);
+
+
+#[allow(missing_docs)]
+pub const MIN: $T = 0 as $T;
+
+#[allow(missing_docs)]
+pub const MAX: $T = !0 as $T;
+
+) }
