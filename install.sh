115232149e44c49074e471dc114b147441429f93
Philipp Oppermann <dev@phil-opp.com>
2016-01-19 12:03:30 +0100

    Add installation script

---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# Exit if anything fails
+set -e
+DIR=$( pwd )
+
+usage="Usage: $0 your-target-name [disable_float]"
+
+if [[ "$#" -lt 1 || "$#" -gt 2 ]]; then
+echo $usage >&2
+exit 1
+fi
+
+target="$1"
+
+if [ "$#" -eq 2 ]; then
+  if [[ "$2" == "disable_float" ]]; then
+    features="--features disable_float"
+  else
+    echo $usage >&2
+    exit 1
+  fi
+else
+  features=""
+fi
+
+extension="${target##*.}"
+if [[ extension == "json" ]]; then
+  targetWithExtension="$target"
+else
+  targetWithExtension="$target.json"
+fi
+
+if multirust which rustc > /dev/null; then
+  rustcDir=$( multirust which rustc )
+elif which rustc > /dev/null; then
+  rustcDir=$( which rustc )
+else
+  echo "Could not detect rust installation!" >&2
+  exit 1
+fi
+
+libraries=$( echo "$rustcDir" | sed s,"bin/rustc","lib/rustlib/$target/lib", )
+
+echo "Installing libcore for $target to"
+echo "$libraries"
+echo ""
+
+git clone --depth 1 https://github.com/phil-opp/nightly-libcore.git
+if [ -f "$targetWithExtension" ]; then
+  cp "$targetWithExtension" "nightly-libcore/$targetWithExtension"
+fi
+cd nightly-libcore
+echo ""
+if cargo build --release $features --target=$target --verbose; then
+  echo ""
+  mkdir -p "$libraries"
+  cp "target/$target/release/libcore.rlib" "$libraries/"
+  cd ..
+  rm -rf nightly-libcore
+  echo "done"
+else
+  echo "Cargo build failed!" >&2
+  cd ..
+  rm -rf nightly-libcore
+  exit 1
+fi
